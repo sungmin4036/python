986b712499d1b9c5bb832335093a393c1d4a1b1e
sungmin4036 <62640332+sungmin4036@users.noreply.github.com>
2022-02-15 00:18:46 +0900

    articleapp_delete,updte

--- django/start_django_2022_02_02/articleapp/models.py
+++ django/start_django_2022_02_02/articleapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)# 지울때 article이 회원 탈퇴시 개시글이 사라지지않고, 알수없음으로 표시되는것/ user객체에서 artice 접근시 사용하는 이름 article
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    created_at = models.DateField(auto_created=True, null=True) # auto_created 자동생성시 시간 자동으로 넣어줌
